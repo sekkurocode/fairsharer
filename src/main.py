35ce0a175b67373f9bf98a9eea73f481ecbfe0b8
Pepe G. Krumminga <pepe.krumminga@study.hs-duesseldorf.de>
2023-11-20 14:50:35 +0100

    Added comments for fair_sharer

--- src/main.py
+++ src/main.py
@@ -17,21 +17,28 @@ def fair_sharer(values, num_iterations, share=0.1):
     # code
     for _ in range(num_iterations):
         
+        # Get the maximum value and it's index in the list. Then calculate the working share
         max_value = max(values)
         idx = values.index(max_value)
         current_share = max_value*share
         
+        # Update the values to reflect the current iteration
+        # Make sure the index does not go out of bounds
         if idx-1 == -1:
             values[len(values)-1] = values[len(values)-1] + current_share
         else:
             values[idx-1] = values[idx-1] + current_share
         
+        # Subtract the distributed amount from the maximum value
         values[idx] = values[idx] - current_share*2
         
+        # Make sure the index does not go out of bounds
         if idx+1 == len(values):
             values[0] = values[0] + current_share
         else:
             values[idx+1] = values[idx+1] + current_share
+            
+        # Make return value    
         values_new = values
             
     return values_new
